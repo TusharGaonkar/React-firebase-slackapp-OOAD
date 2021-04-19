40cf4d31c74ed3a8a518f9a82b26a0e74aa840ab
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2021-04-19 21:26:11 +0530

    Added firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAgIZWEa-RLjZOiqdruRwPgpK91wNAHDhw",
+    authDomain: "ooad-react-firebase-chatapp.firebaseapp.com",
+    projectId: "ooad-react-firebase-chatapp",
+    storageBucket: "ooad-react-firebase-chatapp.appspot.com",
+    messagingSenderId: "511288506008",
+    appId: "1:511288506008:web:25fb23150efbd2063602ed",
+    measurementId: "G-MCH4Z4602Y"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  export default firebase;
